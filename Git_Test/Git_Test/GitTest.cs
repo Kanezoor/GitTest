92ce4d27ed2a45053d4575851ee1c09dba346ea9
Kane <k4nezoor@gmail.com>
2015-11-08 22:42:28 +0200

    first comm

---
+++ Git_Test/Git_Test/GitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Test
+{
+    public class GitTest
+    {
+        public int a=1;
+        public int b=2;
+        public int mult = 3;
+        public int c ;
+
+        public int Addx(int x, int y)
+        {
+            c = a + b;
+            return c;
+        }
+
+        
+    }
+    
+}
---
+++ Git_Test/Git_Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GitTest test = new GitTest();
+            test.a = Convert.ToInt32(Console.ReadLine());
+            test.b = Convert.ToInt32(Console.ReadLine());
+            test.Addx(test.a, test.b);
+            Console.WriteLine(test.c);
+         
+        }
+    }
+}
